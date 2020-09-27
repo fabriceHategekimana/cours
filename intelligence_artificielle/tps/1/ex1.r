d2f3750f5459754da9e46f8d778132a74907e1b0
fabricehategekimana <fab.hatege15@gmail.com>
2020-09-28 00:45:33 +0200

    update

--- intelligence_artificielle/tps/1/ex1.r
+++ intelligence_artificielle/tps/1/ex1.r
@@ -2,6 +2,7 @@
 
 teste<-function(etat){
 	res= TRUE
+	#1ER TESTE
 	#la fonction doit voir s'il n'y a aucun chiffre négatif (qu'on a enlevé des personnes en trop)
 	i=1
        while(i <= 4){
@@ -10,11 +11,33 @@ teste<-function(etat){
 	       }
 		i= i+1	       
        }	       
+       #2E TESTE
 	#La fonction doit vérifier qu'il n'y ait pas plus de cannibal que de missionnaire
        if((etat[1] > 0 & etat[2] > etat[1]) | (etat[3] > 0 & etat[4] > etat[3])){
 		res= FALSE
        }
+       #3E TESTE
+       #la fonction doit contrôler que l'etat n'ai pas déjà été exploré
        res
+}
+
+estDans<-function(vect, mat){
+	if(length(mat) == 0){
+		res=TRUE
+	} else{
+		res=FALSE
+		i= 1
+		while(i<=dim(mat)[1]){
+			#on prend la ligne
+			ligne= mat[i,]
+			#on teste si c'est pareil
+			if(all(vect == ligne)){
+				res=TRUE
+			}
+			i= i+1
+		}
+	}
+	res
 }
 
 #--------------
@@ -26,7 +49,7 @@ a= c(c(-2,0,2,0),c(0,-2,0,2),c(-1,-1,1,1),c(-1,0,1,0),c(0,-1,0,1))
 transitions= matrix(a, byrow=TRUE, ncol=4)
 
 #chemin qui représente la série de noeud qui amènera à la solution
-chemin= c(3,3,0,0)
+chemin= matrix(c(3,3,0,0), byrow=TRUE, ncol=4)
 
 #compteur qui sera attribué à chaque noeuds de la recheche
 compteurs= c(1)
@@ -39,6 +62,8 @@ allezretour= c(1)
 
 arrivee= c(3,0,0,3)
 
+dejaExplore= c()
+
 #-------------
 #DÉBUT DU CODE
 #------------
@@ -46,10 +71,8 @@ arrivee= c(3,0,0,3)
 recherche= TRUE
 while(recherche == TRUE){
 	#on affiche la situation
-	print("compteur")
-	print(compteurs)
 	#On prend le dernier noeud enregistré (le plus à droite)
-	etatActuel= chemin[(length(chemin)-3):length(chemin)]
+	etatActuel= chemin[dim(chemin)[1],]
 	#on prend le dernier compteur enregistré	
 	iActuel= compteurs[length(compteurs)]		
 	mouvementActuel= allezretour[length(allezretour)]
@@ -66,39 +89,32 @@ while(recherche == TRUE){
 	} else{
 		# si on est pas au bout, on continue
 		#on prend la prochaine transition
-		#print("état actuel")
-		#print(etatActuel)
-		#print("mouvement actuel")
-		#print(mouvementActuel)
 		nouvelleTransition= transitions[iActuel,]
-		#print("Nouvelle transition:")
-		#print(nouvelleTransition)
 		nouvelEtat= etatActuel+(nouvelleTransition*mouvementActuel)
-		#print("Nouvel état:")
-		#print(nouvelEtat)
 		#on teste le nouvel état
 		if(teste(nouvelEtat)){
-			print("état valide")
-			#si le teste est positif on enregistre
-			chemin= c(chemin, nouvelEtat)
-			compteurs= c(compteurs,1)
-			mouvementActuel= mouvementActuel-(2*mouvementActuel)
-			allezretour= c(allezretour, mouvementActuel)
-			if(all(nouvelEtat==arrivee)){
-				recherche= FALSE
+			#si le teste est positif on teste
+			#si le noeud a déja été exploré, on cherche ailleurs
+			if(estDans(nouvelEtat, dejaExplore)){
+				compteurs[length(compteurs)]= iActuel+1
+			} else{
+				chemin= rbind(chemin, nouvelEtat)
+				compteurs= c(compteurs,1)
+				mouvementActuel= mouvementActuel-(2*mouvementActuel)
+				allezretour= c(allezretour, mouvementActuel)
+				#si le noeud n'a pas déjà été exploré
+				if(all(nouvelEtat==arrivee)){
+					recherche= FALSE
+				}
 			}
 		} else{
-			print("état non validé")
 			#si le teste est négatif on passe à la transition suivante
 			compteurs[length(compteurs)]= iActuel+1
-			print("compteur")
-			print(compteurs)
 		}
 	}
 	#On change l'orientation (0 = allez) (le plus à droite)
 }
 #On affiche le résultat
 print(chemin)
-print(compteurs)
-print(allezretour)
+
 
